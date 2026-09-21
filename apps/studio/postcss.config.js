3dbdd8e5a41502ea66bbbdd56b04841114ae4dc8
pedrohgdp <pedro.henrique.gp@outlook.com>
2026-09-20 22:59:31 -0300

    FIX: Corrigindo bug do tailwind nao funcionando

---
+++ apps/studio/postcss.config.js
@@ -0,0 +1,8 @@
+// apps/web/postcss.config.js
+module.exports = {
+  plugins: {
+    'tailwindcss/nesting': {}, // Opcional: ajuda se você usa CSS aninhado
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
